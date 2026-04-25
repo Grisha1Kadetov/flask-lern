b5e92795266d23af78533217b4d432d918737f3e
Gregory Kadetov <Gr_kad@mail.ru>
2026-04-25 10:22:51 +0300

    lab2

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import random
-from flask import Flask, render_template
+import re
+from flask import Flask, render_template, request
 from faker import Faker
 
 fake = Faker()
@@ -34,6 +35,25 @@ def generate_post(i):
 
 posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'], reverse=True)
 
+def validate_phone(phone):
+    allowed_symbols = re.compile(r'^[0-9\s().+\-]*$')
+
+    if not allowed_symbols.match(phone):
+        return 'Недопустимый ввод. В номере телефона встречаются недопустимые символы.', None
+
+    digits = re.sub(r'\D', '', phone)
+    phone_start = phone.strip()
+    required_length = 11 if phone_start.startswith('+7') or phone_start.startswith('8') else 10
+
+    if len(digits) != required_length:
+        return 'Недопустимый ввод. Неверное количество цифр.', None
+
+    if required_length == 11:
+        digits = digits[1:]
+
+    formatted_phone = f'8-{digits[:3]}-{digits[3:6]}-{digits[6:8]}-{digits[8:10]}'
+    return None, formatted_phone
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -46,6 +66,35 @@ def posts():
 def post(index):
     p = posts_list[index]
     return render_template('post.html', title=p['title'], post=p)
+
+@app.route('/request-info', methods=['GET', 'POST'])
+def request_info():
+    return render_template(
+        'request_info.html',
+        title='Данные запроса',
+        url_params=request.args.items(multi=True),
+        headers=request.headers.items(),
+        cookies=request.cookies.items(),
+        form_params=request.form.items(multi=True)
+    )
+
+@app.route('/phone', methods=['GET', 'POST'])
+def phone():
+    phone_number = ''
+    error = None
+    formatted_phone = None
+
+    if request.method == 'POST':
+        phone_number = request.form.get('phone', '')
+        error, formatted_phone = validate_phone(phone_number)
+
+    return render_template(
+        'phone.html',
+        title='Проверка телефона',
+        phone_number=phone_number,
+        error=error,
+        formatted_phone=formatted_phone
+    )
 
 @app.route('/about')
 def about():
