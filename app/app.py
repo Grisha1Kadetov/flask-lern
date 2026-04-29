703c2135bb67a8660507dfe5cabdd64e05ceb6e8
Gregory Kadetov <Gr_kad@mail.ru>
2026-04-29 13:14:01 +0300

    lab3

--- app/app.py
+++ app/app.py
@@ -1,12 +1,37 @@
 import random
 import re
-from flask import Flask, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from faker import Faker
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
 
 fake = Faker()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lab-secret-key'
 application = app
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа к запрашиваемой странице необходимо пройти процедуру аутентификации.'
+login_manager.login_message_category = 'warning'
+
+class User(UserMixin):
+    def __init__(self, user_id, login, password):
+        self.id = user_id
+        self.login = login
+        self.password = password
+
+users = {
+    'user': User('1', 'user', 'qwerty')
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users.values():
+        if user.id == user_id:
+            return user
+    return None
 
 images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
               '2d2ab7df-cdbc-48a8-a936-35bba702def5',
@@ -95,6 +120,44 @@ def phone():
         error=error,
         formatted_phone=formatted_phone
     )
+
+@app.route('/visits')
+def visits():
+    session['visits'] = session.get('visits', 0) + 1
+    return render_template('visits.html', title='Счётчик посещений', visits=session['visits'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        login_value = request.form.get('login', '')
+        password = request.form.get('password', '')
+        remember = request.form.get('remember') == 'on'
+        user = users.get(login_value)
+
+        if user and user.password == password:
+            login_user(user, remember=remember)
+            flash('Вы успешно вошли в систему.', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('index'))
+
+        flash('Неверно введён логин или пароль.', 'danger')
+
+    return render_template('login.html', title='Вход')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из системы.', 'info')
+    return redirect(url_for('index'))
+
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html', title='Секретная страница')
 
 @app.route('/about')
 def about():
